7140dea50ad6252121e4ab0199827b28705b5ab4
ula9 <t.ulugbek.9@gmail.com>
2017-02-21 23:48:42 +0500

    it's working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import _ from 'lodash';
 import jpeg from 'jpeg-js';
-import fs from 'mz/fs';
+import fs from 'fs';
 import path from 'path';
 
-const avg = (...args) => {
-  const sum = args.reduce((acc, e) => {
+const avg = (args) => {
+  const sum = args.slice(0, -1).reduce((acc, e) => {
     const newAcc = acc + e;
     return newAcc;
   }, 0);
-
   return sum / args.length;
 };
 
-const pixelToGrayscale = (...rgb) => avg(rgb);
+const pixelToGrayscale = rgb => (avg(rgb.slice(0, -1)));
 
 const grayscaleToChar = (grade) => {
   if (grade < 25) {
@@ -39,9 +38,12 @@ const grayscaleToChar = (grade) => {
 };
 
 
-const jpegToText = async (file) => {
-  const jpegData = await fs.readFile(file);
+/*const jpegToText = (file) => {
+  const jpegData = fs.readFileSync(file);
+  console.log(`JPEG DATA: \n ${jpegData}`);
+  console.log('END JPEG DATA');
   const rawJpegData = jpeg.decode(jpegData);
+  console.log(`JPEG RAW DATA: \n ${rawJpegData}`);
   const pixelArray = _.chunk(rawJpegData, 4);
   const grayscaleJpeg = pixelArray.map(pixelToGrayscale);
   const charJpeg = grayscaleJpeg.map(grayscaleToChar);
@@ -49,8 +51,19 @@ const jpegToText = async (file) => {
   const evenRows = jpegRows.filter((e, index) => index % 2 === 0);
 
   return evenRows.join('\n');
-};
+};*/
 
 export default (file) => {
-  console.log(file);
+  const fileName = path.basename(file, '.jpg');
+  const outputFileName = `${fileName}.txt`;
+  const jpegData = fs.readFileSync(file);
+  const jpegRawData = jpeg.decode(jpegData, true);
+  const pixelArray = _.chunk(jpegRawData.data, 4);
+  const grayscaleJpeg = pixelArray.map(pixelToGrayscale);
+  const charJpeg = grayscaleJpeg.map(grayscaleToChar);
+  const jpegRows = _.chunk(charJpeg, jpegRawData.width).map(e => e.join(''));
+  const evenRows = jpegRows.filter((e, index) => index % 2 === 0);
+  const textPic = evenRows.join('\n');
+  fs.writeFile(outputFileName, textPic, 'utf-8');
+  return `Jpeg has been transformed to ${outputFileName}`;
 };
